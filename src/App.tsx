028782df2b4ccadf1c76cab6d21f23b6a63ce681
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2022-09-06 22:32:46 +0200

    Add a use case for the generic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import Form from "./Form";
+import FormInput from "./FormInput";
 
 const App: React.FC = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
-        <Form
-          label=""
-          type={"text"}
-          id={"Name"}
-          name={"Name"}
-          placeholder={"Name"}
-          onSubmit={() => alert("form submited! :)")}
-        />
+        <Form onSubmit={() => alert("Form submitted!")}>
+          <FormInput
+            type="text"
+            id="firstName"
+            name="First Name"
+            label="First Name"
+            placeholder="First Name"
+          />
+          <FormInput
+            type="text"
+            id="lastName"
+            name="Last Name"
+            label="Last Name"
+            placeholder="Last Name"
+          />
+          <FormInput
+            type="text"
+            id="city"
+            name="City"
+            label="City"
+            placeholder="City"
+          />
+        </Form>
       </header>
     </div>
   );
